10bc746914183c9c7010cbbfad4d631d1b8fab50
milomacphail <milomacphail@gmail.com>
2020-02-21 11:53:29 -0500

    API test route works

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const dotenv = require('dotenv');
-require ('cors');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+const graduates = require("./routes/api/graduates");
 
 
+app.use(cors());
+app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Connected to MongoDB");
+})
+
+app.use('/api/graduates', graduates);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
 
 
